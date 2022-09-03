c6812140d2fb7159a97268f6520cad23430d5125
Tomy Hsieh <tomy0000000@gmail.com>
2022-09-03 17:29:09 +0800

    🗄 chore: Configure mongoid for production

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module Appshelf
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.orm :mongoid
+    end
   end
 end
