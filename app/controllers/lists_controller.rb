38e256f5ad11817e6b5faa28fd68fbc62514a605
Tomy Hsieh <git@tomy.me>
2023-04-23 02:58:25 +0700

    🛠 fix: Confirm before deletion

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -53,7 +53,7 @@ class ListsController < ApplicationController
     @list.destroy
 
     respond_to do |format|
-      format.html { redirect_to lists_url, notice: 'List was successfully destroyed.' }
+      format.html { redirect_to lists_url, status: :see_other, notice: 'List was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
