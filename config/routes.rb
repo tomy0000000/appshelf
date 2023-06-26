0dec6aab1048e00d18622739f398b780021c36ad
Tomy Hsieh <git@tomy.me>
2023-06-26 15:06:24 +0800

    🛠 fix: Username with dots
    close #19

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,11 @@ Rails.application.routes.draw do
   get '/lists/:id/add', to: 'lists#add', as: 'list_add'
   post '/lists/:id/add', to: 'lists#find_app'
   resources :entries
-  resources :users, param: :username
-  get '/users/:username/bye', to: 'users#bye', as: 'user_bye'
-  delete '/users/:username/google_revoke', to: 'users#google_revoke', as: 'user_google_revoke'
+
+  resources :users, param: :username, constraints: { username: %r{[^/]+} }
+  get '/users/:username/bye', to: 'users#bye', as: 'user_bye', constraints: { username: %r{[^/]+} }
+  delete '/users/:username/google_revoke', to: 'users#google_revoke', as: 'user_google_revoke',
+                                           constraints: { username: %r{[^/]+} }
 
   get '/privacy', to: 'application#privacy'
 
